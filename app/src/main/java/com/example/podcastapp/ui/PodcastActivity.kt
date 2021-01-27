05fabe1a5830bdd7e08435ec30a4748483ae2691
SnexusG <swapnilgore029@gmail.com>
2021-01-27 23:22:38 +0530

    search view added

--- app/src/main/java/com/example/podcastapp/ui/PodcastActivity.kt
+++ app/src/main/java/com/example/podcastapp/ui/PodcastActivity.kt
@@ -1,8 +1,12 @@
 package com.example.podcastapp.ui
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
 import com.example.podcastapp.R
 import com.example.podcastapp.Service.ItunesService
 import com.example.podcastapp.repository.ItunesRepo
@@ -15,6 +19,20 @@ class PodcastActivity : AppCompatActivity() {
         val TAG = javaClass.simpleName
         val itunesService = ItunesService.instance
         val itunesRepo = ItunesRepo(itunesService)
-        itunesRepo.searchByTerm("Android Developer", { Log.i(TAG,   "RESULTS : $it)")})
+        itunesRepo.searchByTerm("Android Developer") { Log.i(TAG,   "RESULTS : $it")}
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+
+        val searchMenuItem = menu.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        return true
     }
 }
