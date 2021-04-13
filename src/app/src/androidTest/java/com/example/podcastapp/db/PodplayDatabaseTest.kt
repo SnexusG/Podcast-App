734a9e563e3e1809b0f25be6712e32eb1cfe6c3e
SnexusG <swapnilgore029@gmail.com>
2021-04-13 23:21:30 +0530

    intrumentation testing

--- src/app/src/androidTest/java/com/example/podcastapp/db/PodplayDatabaseTest.kt
+++ src/app/src/androidTest/java/com/example/podcastapp/db/PodplayDatabaseTest.kt
@@ -1,19 +1,19 @@
 package com.example.podcastapp.db
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import androidx.test.platform.app.InstrumentationRegistry
+import com.example.podcastapp.getOrAwaitValue
 import com.example.podcastapp.model.Episode
 import com.example.podcastapp.model.Podcast
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import org.junit.After
-import org.junit.Assert
-import org.junit.Before
-import org.junit.Test
+import org.junit.*
+import org.junit.rules.TestRule
 import org.junit.runner.RunWith
 import java.util.*
 
@@ -25,6 +25,9 @@ class PodplayDatabaseTest {
     private lateinit var podcastDB : PodPlayDatabase
     private lateinit var podcastDBDao : PodcastDao
     private lateinit var date : Date
+
+    @get:Rule
+    var instanctTaskExecutorRule = InstantTaskExecutorRule()
 
     @Before
     fun setup(){
@@ -44,11 +47,22 @@ class PodplayDatabaseTest {
     @Test
     fun EpisodeTest() = runBlocking {
         val episode = Episode("1", 1, "episode title", "episode description", "mediaURL", "mimeType", date, "duration")
-        val episodesList:List<Episode> = listOf(Episode("1", 1, "episode title", "episode description", "mediaURL", "mimeType", date, "duration"))
+        val episodesList:List<Episode> = listOf(episode)
         val podcast = Podcast(1, "some url", "some title", "some description", "image.jpeg", date, episodesList)
         podcastDBDao.insertPodcast(podcast)
-        podcastDBDao.insertEpisode(Episode("1", 1, "episode title", "episode description", "mediaURL", "mimeType", date, "duration"))
+        podcastDBDao.insertEpisode(episode)
         val loadpodcastEpisodes = podcastDBDao.loadEpisdoes(1)
         assertThat(loadpodcastEpisodes).contains(episode)
+    }
+
+    @Test
+    fun deleteTest() = runBlocking{
+        val episode = Episode("1", 1, "episode title", "episode description", "mediaURL", "mimeType", date, "duration")
+        val episodesList:List<Episode> = listOf(episode)
+        val podcast = Podcast(1, "some url", "some title", "some description", "image.jpeg", date, episodesList)
+        podcastDBDao.insertPodcast(podcast)
+        podcastDBDao.deletePodcast(podcast)
+        val podcastC = podcastDBDao.loadPodcast().getOrAwaitValue()
+        assertThat(podcastC).isEmpty()
     }
 }
